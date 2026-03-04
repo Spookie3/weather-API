6213e3b0f8d618217385d5b11943fd4d5c69ca39
Jonathan <jonathan_johnson1@hotmail.com>
2026-03-04 12:24:59 +0100

    re-added inital CSS & added testlogs to script

--- js/hourlyForecast.js
+++ js/hourlyForecast.js
@@ -1,19 +1,40 @@
-let API_key = "";
 let hourlyCards = document.getElementById("hourlyForecast");
+let API_key = "80948121ac889b120dca64a6c7e5f24c";
+let latitude = "";
+let longitude = "";
+let srcelectedDay = 0 * 24;
+let tempUnit = "C";
+const day = localStorage.getItem("day");
 
 
-async function getHourlyForecastData(lat, lon, API_key) {
-    let forecast_API = `https://api.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${API_key}`;
-    const data = await fetch(forecast_API);
-    const weatherData = await data.json();
-    const hourlyData = weatherData.list;
-    for(i=0; i < 24; i++) {
-        hourlyCards.innerHTML += `
-            <div class="forecast-card">
-                <p>${hourlyData[i].dt}</p>
-                <img src="https://openweathermap.org/img/wn/${hourlyData[i].weather.icon}.png" alt="current weather: ${hourlyData[i].weather.description}">
-                <p>${hourlyData[i].main.temp} &deg;C</p>
-            </div>
-        `;
+async function getHourlyForecastData(lat, lon, API_key, tempSelector, selectedDay) {
+    let forecast_API = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${API_key}&units=${tempSelector}`;
+    try {
+        const response = await fetch(forecast_API);
+        if (!response.ok) {
+            throw new Error("Server issue: " + response.status);
+        }
+        console.log("forecast data was retrieved")
+        const weatherData = await response.json();
+        const hourlyData = weatherData.list;
+        if(tempSelector==="imperial"){
+            tempUnit = "F";
+        } else {
+            tempUnit = "C";
+        }
+        for(i=0+selectedDay; i < 24+selectedDay; i++) {
+            hourlyCards.innerHTML += `
+                <div class="forecast-card">
+                    <p>${hourlyData[i].dt}</p>
+                    <img src="https://openweathermap.org/img/wn/${hourlyData[i].weather.icon}.png" alt="current weather: ${hourlyData[i].weather.description}">
+                    <p>${hourlyData[i].main.temp} &deg;${tempUnit}</p>
+                </div>
+            `;
+        }
+    } catch (error) {
+        console.log(error);
     }
+    
 }
+
+getHourlyForecastData(59.609901, 16.544809, API_key, "metric", 0);
